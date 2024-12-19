8d2c7428291abd0d72fdbf639bfe0f48f1bb8b05
Chanfreau <cedric.chanfreau@gmail.com>
2024-12-19 12:12:29 +0100

    Modify Battery Display

--- Screen/GetBattery.js
+++ Screen/GetBattery.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import data from '../Config/data.json';
 
-
 export default function GetBattery({ nodeNumber }) {
-    return(
+    const batteryLevel = data.nodes[nodeNumber].batterylevel;
+    let batteryIcon = 'battery-full';
+    let batteryColor = '#45a049';
+
+    if (batteryLevel < 20) {
+        batteryIcon = 'battery-empty';
+        batteryColor = 'red';
+    } else if (batteryLevel < 40) {
+        batteryIcon = 'battery-quarter';
+        batteryColor = 'orange';
+    } else if (batteryLevel < 60) {
+        batteryIcon = 'battery-half';
+        batteryColor = 'yellow';
+    } else if (batteryLevel < 80) {
+        batteryIcon = 'battery-three-quarters';
+        batteryColor = '#45a049';
+    }
+
+    return (
         <View style={styles.container}>
-            <Text style={[styles.infoValue, 
-                data.nodes[nodeNumber].batterylevel < 20 && { color: 'red' }]}>
-                {data.nodes[nodeNumber].batterylevel} %
+            <Icon name={batteryIcon} size={24} color={batteryColor} style={styles.icon} />
+            <Text style={[styles.infoValue, { color: batteryColor }]}>
+                {batteryLevel} %
             </Text>
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
@@ -19,10 +37,14 @@ const styles = StyleSheet.create({
         flexGrow: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        flexDirection: 'row',
+    },
+    icon: {
+        transform: [{ rotate: '-90deg' }],
     },
     infoValue: {
-        fontSize: 24,
+        fontSize: 20,
         fontWeight: 'bold',
-        color: '#45a049',
+        marginLeft: 10,
     },
 });
